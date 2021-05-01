2daa993bfa8a4b3ee162e092249f5c303dab4954
Ryan Pope <rcpope7@gmail.com>
2021-04-30 21:09:05 -0400

    Added purchase info + other small stuff
    - Removed password from profile page
    - Added "purchase info" to right side of canvas page which updates dynamically as pixels are selected
    - Moved paypal button to "purchase info", but it still needs to be updated to have the correct amount based on pixels selected
    - Added charity dropdown
    - Added some underlying data structures to better track what pixels are selected and how many

--- ajax/app.js
+++ ajax/app.js
@@ -7,6 +7,9 @@ const colsInput = 20;
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
 
+let gridInfo = [];
+let totalSelected = 0;
+
 function makeGrid(numRows, numCols, color) {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -14,6 +17,13 @@ function makeGrid(numRows, numCols, color) {
     ctx.strokeStyle = color || "black";
     let width = canvasWidth / numCols;
     let height = canvasHeight / numRows;
+
+    for(let i = 0; i < numRows; i++){
+        gridInfo[i] = [];
+        for(let j = 0; j < numCols; j++){
+            gridInfo[i][j] = {color:"white", charity:"None", selected:false}
+        }
+    }
 
     for (let i = width; i < canvasWidth; i += width) {
         drawLine(i, 0, i, canvasHeight);
@@ -38,6 +48,17 @@ function init(){
 
     makeGrid(numRows, numCols, "black");
 
+    function findIndex(num, size) {
+        num = num - (num % size);
+        return (num === 0) ? num : num + 1;
+    }
+
+    function pixelCoords(posX, posY){
+        let x = canvasWidth / colsInput;
+        let y = canvasHeight / rowsInput;
+        return [Math.floor(posX/x), Math.floor(posY/y)]
+    }
+
     canvas.onclick = function(event){
         event.preventDefault();
         
@@ -46,14 +67,20 @@ function init(){
         let y = event.clientY - margin.top;
         
         let colorInput = document.getElementById("color-list");
-        drawSquare(x, y, colorInput.value);
+        let pxs = pixelCoords(findIndex(x, canvasWidth / numCols), findIndex(y, canvasHeight / numRows))
+        let pixel = gridInfo[pxs[1]][pxs[0]]
+        if(!pixel.selected){
+            drawSquare(x, y, colorInput.value);
+        }
     }
 
     function drawSquare(x, y, color){
         ctx.fillStyle = color || "white";       
     
         let squareWidth = canvasWidth / numCols;
-        let squareHeight = canvasHeight / numRows; 
+        let squareHeight = canvasHeight / numRows;
+
+        console.log(x, y);
 
         x = findIndex(x, squareWidth);
         y = findIndex(y, squareHeight); 
@@ -65,11 +92,18 @@ function init(){
         squareHeight -= (onHorizonalAxis) ? 1 : 2;
     
         ctx.fillRect(x, y, squareWidth, squareHeight);
+        let pxs = pixelCoords(x, y)
 
-        function findIndex(num, size) {
-            num = num - (num % size);
-            return (num === 0) ? num : num + 1;
-        }
+        let chr = document.getElementById("charity-list").value;
+        let pixel = gridInfo[pxs[1]][pxs[0]]
+
+        pixel.color = color
+        pixel.charity = chr
+        pixel.selected = true
+        totalSelected++;
+        $("#total-num").html(totalSelected)
+
+        $("#purchase-contents").append("<div class='item'><span class='item-pixel' style='background-color:"+color+"'></span><span class='item-text'>@ ("+pxs[1]+", "+pxs[0]+") supporting "+chr+" </span><span class='cost'>$1</span></div><hr/>")
     }
 }
 
--- ajax/home.php
+++ ajax/home.php
@@ -10,6 +10,12 @@ $sqlColor = "SELECT * FROM `Colors`";
 // make the query and get the result
 $colorResult = mysqli_query($con, $sqlColor);
 
+// query for colors
+$sqlCharity = "SELECT * FROM `Charities`";
+
+// make the query and get the result
+$charityResult = mysqli_query($con, $sqlCharity);
+
 // close connection
 mysqli_close($con);
 
@@ -27,30 +33,49 @@ if (!isset($_SESSION['loggedin'])) {
 		<title>Pixels for Humanity</title>
 		<link href="styles/css/login.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+        <script src="js/jquery-1.6.2.min.js"></script>
 	</head>
 	<body class="loggedin">
 		<?php
         include("base.php")
         ?>
-		<div class="content">
-            <h2>Pick a color or a charity of your choice</h2>
+        <div id="grid-container">
+		<div class="content" id="main-content">
+            <h2>Select a color and a charity of your choice</h2>
             <div class="container">
                 <?php
-                    echo "<select name='color-list' id='color-list'>";
+                    echo "<span id='colorsel'><label id='color-label'>Color:</label><select name='color-list' id='color-list'>";
                     while ($row = mysqli_fetch_array($colorResult)) {
                         echo "<option id='color-input' value='" . $row['hexcode'] . "'>" . $row['color_name'] . "</option>";
                     }
-                    echo "</select>";
+                    echo "</select></span>";
+                    echo "<span id='charity'><label id='charity-label'>Charity:</label><select name='charity-list' id='charity-list'>";
+                    while ($row = mysqli_fetch_array($charityResult)) {
+                        echo "<option id='charity-input' value='" . $row['charity_name'] . "'>" . $row['charity_name'] . "</option>";
+                    }
+                    echo "</select></span>";
                 ?>
             </div>
 
             <canvas id="pixelCanvas" width="1000" height="1000"></canvas>
-            <form action="https://www.sandbox.paypal.com/donate" method="post" target="_top">
-            <input type="hidden" name="hosted_button_id" value="HYSZKYCADBDKW" />
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-            <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-            </form>
+            
             <script src="app.js"></script>
 		</div>
+        <div id="purchase-info">
+            <div id="purchase-header">Purchase Info</div>
+            <hr />
+            <div id="purchase-contents">
+            </div>
+            <hr />
+            <div id="purchase-bottom">
+                <form action="https://www.sandbox.paypal.com/donate" method="post" target="_top">
+                    <input type="hidden" name="hosted_button_id" value="HYSZKYCADBDKW" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+                <div id="purchase-total"><strong>Order Total</strong><br/>$<span id="total-num">0</span>.00</div>
+            </div>
+        </div>
+    </div>
 	</body>
 </html>
