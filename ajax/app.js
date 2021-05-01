ad01c97e956c1f4e1ec890196c55c1b61ff187ed
Kyle Cheng <kwscheng@gmail.com>
2021-05-01 18:01:14 -0400

    a

--- ajax/app.js
+++ ajax/app.js
@@ -48,11 +48,13 @@ function init(){
     let numCols = 20;
 
     makeGrid(numRows, numCols, "black");
-    
+
     for(let i = 0; i < numRows; i++){
         gridInfo[i] = [];
         for(let j = 0; j < numCols; j++){
             drawSquare[i,j,gridInfo[i][j].color]
+            gridInfo[i][j].charity = "None"
+            gridInfo[i][j].selected = false
         }
     }
     function findIndex(num, size) {
