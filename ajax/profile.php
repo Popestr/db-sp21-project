3f58e039cff415941093fd3755bf325dac1fb664
mk4ed <minportant@gmail.com>
2021-05-03 18:43:48 -0400

    latest commit

--- ajax/profile.php
+++ ajax/profile.php
@@ -21,7 +21,7 @@ WHERE p.purchaser_id=? ORDER BY purchase_date DESC");
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
 
-	$stmt_charities = mysqli_prepare($con, "SELECT charity_name, subtotal FROM `Charity_Admins` ca LEFT OUTER JOIN `Charities` c ON c.charity_id = ca.charity_id WHERE admin_id = ?");
+	$stmt_charities = mysqli_prepare($con, "SELECT charity_name, subtotal, c.charity_id FROM `Charity_Admins` ca LEFT OUTER JOIN `Charities` c ON c.charity_id = ca.charity_id WHERE admin_id = ?");
     
 } catch (mysqli_sql_exception $exception) {
     throw $exception;
@@ -67,31 +67,21 @@ $feedbackResult = mysqli_query($con, $sqlFeedback);
 					</tr>
 				</table>
 			</div>
-			<h2>Purchases</h2>
+			
+			<h2>Purchases
+				<input class="xlarge" id="charityinput" type="search" size="30" value="" placeholder="Charity name contains"></h2>
 			<?php if(mysqli_stmt_num_rows($stmt_purch) == 0): ?>
 				<div id="purchases-header"> It looks like you don't have any purchases yet.</div>
 			<?php else: ?>
-			<table id="user-purchases">
-				<tr><th>Date</th><th>Pixel ID</th><th>Pixel Color</th><th>Charity</th></tr>
-				<?php 
-					while (mysqli_stmt_fetch($stmt_purch)) {
-						echo "<tr><td>".$pdate."</td><td>".$pixid."</td><td>".$color."</td><td>".$charname."</td></tr>";
-					}
-					mysqli_stmt_close($stmt_purch);
-					mysqli_stmt_execute($stmt_charities);
-					mysqli_stmt_bind_result($stmt_charities, $cname, $subtotal);
-					mysqli_stmt_store_result($stmt_charities);
-				?>
-			</table>
-			<?php endif ?>
-
-			<h2>Purchases filterable</h2>
 			<script>
 				$(document).ready(function() {
 					$( "#charityinput" ).change(function() {
 						$.ajax({
 							url: 'purchasesajax.php', 
 							data: {charity_name: $( "#charityinput" ).val()},
+							beforeSend: function(){
+								$('.loading-overlay').show();
+							},
 							success: function(data){
 								$('#charityresult').html(data);	
 							}
@@ -100,40 +90,56 @@ $feedbackResult = mysqli_query($con, $sqlFeedback);
 				});
 			</script>
 			<div>
-				<input class="xlarge" id="charityinput" type="search" size="30" placeholder="Charity name contains">
-				<div id="charityresult"></div>
+				<div id="charityresult">
+					<table id="user-purchases">
+						<tr><th>Date</th><th>Pixel ID</th><th>Pixel Color</th><th>Charity</th></tr>
+						<?php 
+							while (mysqli_stmt_fetch($stmt_purch)) {
+								echo "<tr><td>".$pdate."</td><td>".$pixid."</td><td>".$color."</td><td>".$charname."</td></tr>";
+							}
+							mysqli_stmt_close($stmt_purch);
+							mysqli_stmt_execute($stmt_charities);
+							mysqli_stmt_bind_result($stmt_charities, $cname, $subtotal, $cid);
+							mysqli_stmt_store_result($stmt_charities);
+						?>
+					</table>
+				</div>
 			</div>
+			<?php endif ?>
 
 			<h2>Charities You Manage<a href="charityrequest.php"><button id="charity-request-button">Request a Charity</button></a></h2>
 			<?php if(mysqli_stmt_num_rows($stmt_charities) == 0): ?>
 				<div id="charity-manage-header"> It looks like you're not managing any charities yet.</div>
 			<?php else: ?>
-			<table id="user-purchases">
-				<tr><th>Charity Name</th><th>Total Pixels Donated</th><th>Export</th></tr>
-				<?php 
-					while (mysqli_stmt_fetch($stmt_charities)) {
-
-						echo "<tr><td>".$cname."</td><td>".$subtotal."</td><td><a href='exportDonatorInfo.php?cid=".$cid."'>Export Donator Info</a></td></tr>";
-					}
-				?>
-			</table>
+				<div>
+					<table id="user-purchases">
+						<tr><th>Charity Name</th><th>Total Pixels Donated</th><th>Export</th></tr>
+						<?php 
+							while (mysqli_stmt_fetch($stmt_charities)) {
+								echo "<tr><td>".$cname."</td><td>".$subtotal."</td><td><a href='exportDonatorInfo.php?cid=".$cid."'>Export Donator Info</a></td></tr>";
+							}
+						?>
+					</table>
+				</div>
 			<?php endif ?>
 
-			<h2>Manage feedbacks here</h2>
-			<?php if(mysqli_num_rows($feedbackResult) == 0): ?>
-				<div id="charity-manage-header"> It looks like no feedbacks were made by you.</div>
-			<?php else: ?>
-				<?php 
-					while($row = mysqli_fetch_array($feedbackResult)){  
-						echo "<div><table>";
-						echo "<tr><td>" . "Username:" . "</td><td>" . $row['username'] . "</td></tr>"; 
-						echo "<tr><td>" . "Title:" . "</td><td>" . $row['title'] . "</td></tr>"; 
-						echo "<tr><td>" . "Comment:" . "</td><td>" . $row['content'] . "</td></tr>"; 
-						echo "<tr><td>" . "<a href='feedbackform.php?edit=" . $row['feedback_id'] . "' class='edit_btn'> Edit </a>" . "</td>"; 
-						echo "<td>" . "<a href='feedback.php?del=" . $row['feedback_id'] . "' class='del_btn'> Delete </a>" . "</td></tr>"; 
-						echo "</table></div>";
-					}
-				?>
+			<?php if($_SESSION['userrole'] == 'hs2fw_b'): ?>
+				<h2>Manage feedbacks here</h2>
+				<?php if(mysqli_num_rows($feedbackResult) == 0): ?>
+					<div id="charity-manage-header"> It looks like no feedbacks were made by you.</div>
+				<?php else: ?>
+					<?php 
+						while($row = mysqli_fetch_array($feedbackResult)){  
+							echo "<div><table>";
+							echo "<tr><td>" . "Username:" . "</td><td>" . $row['username'] . "</td></tr>"; 
+							echo "<tr><td>" . "Title:" . "</td><td>" . $row['title'] . "</td></tr>"; 
+							echo "<tr><td>" . "Comment:" . "</td><td>" . $row['content'] . "</td></tr>"; 
+							echo "<tr><td>" . "<a href='feedbackform.php?edit=" . $row['feedback_id'] . "' class='edit_btn'> Edit </a>" . "</td>"; 
+							echo "<td>" . "<a href='feedback.php?del=" . $row['feedback_id'] . "' class='del_btn'> Delete </a>" . "</td></tr>"; 
+							echo "</table></div>";
+						}
+					?>
+				<?php endif ?>
 			<?php endif ?>
 		</div>
 	</body>
